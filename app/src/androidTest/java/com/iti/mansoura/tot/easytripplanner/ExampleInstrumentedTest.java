541c138bc6ed5426739373bc51a3b73ca3109a9a
Fahmi <fahmymostafa.f@gmail.com>
2019-10-15 15:14:13 +0200

    V 1.3 Renamed Packages AddTrip Process with Stepper and Storing Trip data in Firebase

--- app/src/androidTest/java/com/iti/mansoura/tot/easytripplanner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/iti/mansoura/tot/easytripplanner/ExampleInstrumentedTest.java
@@ -3,7 +3,7 @@ package com.iti.mansoura.tot.easytripplanner;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.zayed.easytripplanner", appContext.getPackageName());
+        assertEquals("com.iti.mansoura.tot.easytripplanner", appContext.getPackageName());
     }
 }
